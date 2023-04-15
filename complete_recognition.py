46ca0d19611e94cd5fe152329de0ed0ad024aed6
R-e-v-a-n-t-h <revanthselvan@gmail.com>
2023-04-15 23:41:45 +0400

    Backend initial code

---
+++ complete_recognition.py
@@ -0,0 +1,94 @@
+from classes import classes
+from ultralytics import YOLO
+import torch
+import numpy as np
+import cv2
+import time
+import easyocr
+
+class CompleteRecognition:
+    def __init__(self,threshold):
+
+        self.model = YOLO("best.pt")
+        self.reader = easyocr.Reader(["en"],gpu=True)
+        self.classes = classes
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.threshold=threshold
+
+    def obj_score_frame(self, frame):
+
+        self.model.to(self.device)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        frame = [frame]
+        results = self.model(frame)
+        # print(results[0].boxes)
+        cls = results[0].boxes.cls.tolist()
+        xyxyn = results[0].boxes.xyxyn.tolist()
+        conf=  results[0].boxes.conf.tolist()
+
+        labels = []
+        cord=[]
+        percent=[]
+        for i in range(len(cls)):
+            if conf[i]>self.threshold:
+                labels.append(classes[int(cls[i])]) #LABELS
+
+                x1, y1, x2, y2 = int(xyxyn[i][0] * x_shape), int(xyxyn[i][1] * y_shape), int(xyxyn[i][2] * x_shape), int(xyxyn[i][3] * y_shape)
+                cord.append([x1,y1,x2,y2])  #BOX DIAGONAL COORDINATES
+
+                percent.append(conf[i]*100) #CONFIDENCE PERCENT
+
+        return labels, cord, percent
+
+    def ocr_score_frame(self,frame):
+        results = self.reader.readtext(frame)
+        labels=[]
+        cords=[]
+        percent=[]
+        for i in range(len(results)):
+            confidence = results[i][2]
+            if confidence > self.threshold:
+                points = results[i][0]
+                label = results[i][1]
+                x1, y1 = points[3]
+                x2, y2 = points[1]
+
+                labels.append(label)
+                cords.append([x1, y1, x2, y2])
+                percent.append(confidence)
+
+        return labels,cords, percent
+
+
+    def __call__(self):
+
+        cap = cv2.VideoCapture(0)
+
+        while cap.isOpened():
+
+            # start_time = time.perf_counter()
+            ret, frame = cap.read()
+            if not ret:
+                break
+            obj_labels, obj_coordinates, obj_confidence = self.obj_score_frame(frame)
+            ocr_labels, ocr_coordinates, ocr_confidence = self.ocr_score_frame(frame)
+
+
+            results={
+                "obj":{
+                    "labels":obj_labels,
+                    "coordinates": obj_coordinates,
+                    "confidence": obj_confidence
+                },
+                "ocr":{
+                    "labels": ocr_labels,
+                    "coordinates": ocr_coordinates,
+                    "confidence": ocr_confidence
+                }
+            }
+            print(results)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+model = CompleteRecognition(0.5)
+model()
---
+++ main.py
@@ -0,0 +1,112 @@
+# BACKEND IMPORTS
+from condition import *
+from fastapi import FastAPI, Path, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from complete_recognition import CompleteRecognition
+
+# DATA HANDLING
+import pandas as pd
+import numpy as np
+import ast
+import pickle
+
+app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# Load models
+detector = CompleteRecognition()
+recipeModel = pickle.load(open("NearestNeighborModel.pkl", 'rb'))
+
+
+@app.on_event("startup")
+async def startup_event():
+    # Store models in the application's state
+    app.state.detector = detector
+    app.state.recipeModel = recipeModel
+
+
+@app.get('/api')
+async def openingPage():
+    data = pd.read_csv('Cleaned_Data.csv')
+    allIngredients = data.columns.tolist()
+    allIngredients.pop(0)
+    allIngredients.pop(0)
+    allIngredients.pop(0)
+    allIngredients.pop(0)
+    allIngredients.pop(0)
+    indecies = {}
+    dishes = np.array(data["title"]).tolist()
+    for i in range(len(allIngredients)):
+        indecies[allIngredients[i]] = i
+    return {"ingredients": allIngredients, "indecies": indecies, "dishes": dishes}
+
+
+@app.get("/api/required-ingredients")
+async def requiredIngredients():
+    data = pd.read_csv('Cleaned_Data.csv')
+    # data.drop_duplicates()
+    ner = np.array(data["NER"]).tolist()
+    x = np.array(data["title"]).tolist()
+    for i in range(len(ner)):
+        ner[i] = ast.literal_eval(ner[i])
+    ingredients = {}
+    for i in range(len(x)):
+        if x[i] not in ingredients:
+            ingredients[x[i]] = ner[i]
+    return ingredients
+
+
+@app.post("/api/specific-recipes")
+async def recipes(request: Request):
+    data = pd.read_csv('Cleaned_Data.csv')
+    if request:
+        params = await request.json()
+        dish = params["dish"]
+    requestedData = data.loc[data["title"].str.contains(dish, regex=False)]
+    directions = ast.literal_eval(
+        np.array(requestedData["directions"]).tolist()[0])
+    ingredients = ast.literal_eval(
+        np.array(requestedData["ingredients"]).tolist()[0])
+
+    return {"ingredients": ingredients, "directions": directions}
+
+
+@app.post("/api/predict")
+async def predict(request: Request):
+
+    data = pd.read_csv('Cleaned_Data.csv')
+    pickle_in = open("NearestNeighborModel.pkl", 'rb')
+    model = pickle.load(pickle_in)
+
+    allIngredients = data.columns.tolist()
+    allIngredients.pop(0)
+    allIngredients.pop(0)
+    allIngredients.pop(0)
+    allIngredients.pop(0)
+    allIngredients.pop(0)
+
+    if request:
+        params = await request.json()
+        X = params["array"]
+        picked = params["chosenIngredients"]
+
+        if len(X) == len(allIngredients):
+            predictions = model.kneighbors(np.array([X]))
+            predictedData = data.iloc[predictions[1][0]]
+            predictedData = classifier(predictedData, picked)
+            sendData = np.array(predictedData["title"]).tolist()
+            return {"predictions": sendData}
+        else:
+            raise HTTPException(400, "Error")
+
+    raise HTTPException(400, "Error")
